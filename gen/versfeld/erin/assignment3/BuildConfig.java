683a85faa0e45df929c7b0a6503d55a7be0ba0c4
erinVersfeld <vrseri001@myuct.ac.za>
2014-09-07 11:06:53 +0200

    7 September 11:06 am - Created AppConstant class to store static variables which will be used across activities.

---
+++ gen/versfeld/erin/assignment3/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package versfeld.erin.assignment3;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/versfeld/erin/assignment3/helper/AppConstant.java
@@ -0,0 +1,19 @@
+package versfeld.erin.assignment3.helper;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AppConstant {
+	
+	//number of columns in which the images are displayed
+	public static final int NUMBER_OF_COLUMNS = 3;
+	
+	//padding between images in the GridView
+	public static final int IMAGE_PADDING = 8;
+	
+	//the image directory on the SD card
+	public static final String PHOTO_ALBUM = "random";
+	
+	//supported file formats
+	public static final List<String> FILE_EXTENSIONS = Arrays.asList("jpg", "jpeg", "png");
+}
